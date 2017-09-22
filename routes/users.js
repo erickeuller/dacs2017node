6b21b2139905eb3e98462f32294bf9352eed6e1b
erickeuller <erick.opelt@univille.edu.br>
2017-09-22 06:52:39 -0300

    Create/edit user

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,42 @@ var dbutil = require('../util/db');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log(req.query);
-  var conn = dbutil();
-  conn.query('select * from tbuser',
-    function(err,rows,fields){
-        console.log(rows);
-        res.render('users',{data:rows});
-        //res.send('mudou qq coisa');
-    }
-  );
+router.get('/', function (req, res, next) {
+    console.log(req.query);
+    var conn = dbutil();
+    conn.query('select * from tbuser',
+        function (err, rows, fields) {
+            res.render('users', {data: rows});
+        }
+    );
 });
 
-router.get('/edit/:id', function(req, res, next) {
-  console.log(req.params);
-  res.send('Chegou aqui:' + req.params.id);
+router.get('/edit/:id', function (req, res, next) {
+    console.log(req.params);
+    var conn = dbutil();
+    conn.query('select * from tbuser where id = ?', [req.params.id], function (req, data) {
+        res.render('user-edit', data[0]);
+    })
+});
+
+router.get('/new', function (req, res, next) {
+    res.render('user');
+});
+
+router.post('/', function (req, res, next) {
+    var conn = dbutil();
+    conn.query('insert into tbuser(login, email, senha) values(?,?,?)',
+        [req.body.login, req.body.email, req.body.senha], function (data) {
+            res.render('users')
+        });
+});
+
+router.post('/:id', function (req, res, next) {
+    var conn = dbutil();
+    conn.query('UPDATE tbuser SET login = ?, senha = ?, email = ? ' +
+        'WHERE id = ?', [req.body.login, req.body.email, req.body.senha, req.params.id], function () {
+            res.render('users')
+    });
 });
 
 module.exports = router;
--- util/db.js
+++ util/db.js
@@ -1,17 +1,20 @@
 var mysql = require('mysql');
-function dbcomm(){
-	var connection = mysql.createConnection({
-	  host     : 'localhost',
-	  user     : 'bob',
-	  password : 'bob',
-	  database : 'nodedacs2017'
-	});
 
-	connection.connect(function(err){
-		if(err) {
-		    console.log("Error connecting database ... nn");    
-		}
-	});
-	return connection;
+function dbcomm() {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nodedacs2017'
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.log("Error connecting database ... nn");
+        }
+    });
+
+    return connection;
 }
+
 module.exports = dbcomm;
